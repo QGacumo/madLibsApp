a2aa83e086a0c4f5984a83f18722522ab1a46820
QGacumo <ryukengacumo@gmail.com>
2022-09-02 11:26:36 +0800

    Added mad-lib replacement variables to story.

---
+++ app.js
@@ -0,0 +1,20 @@
+"use strict";
+
+let originalStory = `In 1837, Charles Babbage proposed the first general mechanical computer, the Analytical Engine. The Analytical Engine contained an ALU (Arithmetic Logic Unit), basic flow control, punch cards (inspired by the Jacquard Loom), and integrated memory`;
+
+const protagonistName = "Charles Babbage";
+
+let userInputs = [];
+
+console.log(originalStory);
+
+/*
+`In 1837, Charles ${userInputs[0]} proposed the first general mechanical ${userInputs[1]}, the Analytical Engine. The ${userInputs[2]} Engine contained an ALU (Arithmetic Logic Unit), basic flow control, punch cards (inspired by the Jacquard Loom), and ${userInputs[3]} memory`;
+*/
+
+
+
+
+
+/*In 1837, Charles Babbage proposed the first general mechanical computer, the Analytical Engine. The Analytical Engine contained an ALU (Arithmetic Logic Unit), basic flow control, punch cards (inspired by the Jacquard Loom), and integrated memory
+*/
